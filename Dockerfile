b79d1f8919ce1b5c3ffdcf535693a22f84398773
Kok How, Teh <funcoolgeek@gmail.com>
2025-04-05 00:26:08 +0800

    Fix run.sh

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,6 @@ RUN apt install -y nvidia-container-toolkit
 ADD run.sh /usr/local/bin/run.sh
 EXPOSE 11434
 ENTRYPOINT ["/usr/local/bin/run.sh"]
+#ENTRYPOINT ["/bin/ollama"]
 CMD ["bash"]
+#CMD ["/bin/ollama", "serve"]
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-ollama serve
+ollama serve &
+pid=$!                       # ADD: save the process ID of the server
 sleep 10
-MODEL="${MODELS//,/ }"
-for i in "${MODEL[@]}"; do \
-      echo model: $i  \
-      ollama pull $i \
-    done
+models="${MODELS//,/ }"
+for i in "${models[@]}"; do
+      echo model: $i
+      ollama pull $i
+done
+wait "$pid"                  # ADD: keep the script running as long as the server is too

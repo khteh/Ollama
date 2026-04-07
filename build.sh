ef5ef554664218a0356dfd2aebdf9ab19dc8c339
Kok How, Teh <funcoolgeek@gmail.com>
2026-04-07 11:04:57 +0800

    Fix BuildKit build with no-cache

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #$(aws ecr get-login --no-include-email)
+#docker build --no-cache --pull -t ollama .
 docker build -t ollama .
 docker tag ollama:latest khteh/ollama:latest
 docker push khteh/ollama:latest
